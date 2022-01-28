9c6d4bd2d4f30f0b8ad70eb28d9a990f8671e3d5
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-01-28 17:08:44 +0000

    Adapted Frontier.print to reflect the expected stacscheck behaviour

--- src/Frontier.java
+++ src/Frontier.java
@@ -9,6 +9,11 @@ public abstract class Frontier {
     public abstract String toString();
     public abstract Iterator<Node> iterator();
     public void print() {
-        System.out.println(this);
+        Iterator<Node> it = iterator();
+        System.out.print("[");
+        while (it.hasNext()) {
+            System.out.print(it.next());
+        }
+        System.out.println("]");
     }
 }
