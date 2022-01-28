8313abcf412fb97f6f2187a7efdf4878ab920dd2
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-01-28 17:03:33 +0000

    Implemented basic tests for search

---
+++ test/SearchTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SearchTest {
+
+    Conf conf = Conf.valueOf("JCONF00");
+    Map map = conf.getMap();
+    Coord start = conf.getS();
+    Coord goal = conf.getG();
+
+    @Test
+    public void testBreadthFirst() {
+        Search search = new BreadthFirstSearch(map, start, goal);
+        search.runSearch();
+    }
+
+    @Test
+    public void testDepthFirst() {
+        Search search = new DepthFirstSearch(map, start, goal);
+        search.runSearch();
+    }
+
+    @Test
+    public void testBestFirst() {
+        Search search = new BestFirstSearch(map, start, goal);
+        search.runSearch();
+    }
+
+    @Test
+    public void testAStar() {
+        Search search = new AStarSearch(map, start, goal);
+        search.runSearch();
+    }
+
+
+
+
+}
