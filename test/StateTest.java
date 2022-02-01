752a32c39530903c62b3e46bb3110575cba1dfe4
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-01 11:08:11 +0000

    Refactored StateTest

--- test/StateTest.java
+++ test/StateTest.java
@@ -1,25 +1,24 @@
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
 
 public class StateTest {
 
+    Map map = Conf.valueOf("JCONF01").getMap();
+    State topLeft = new State(new Coord(0, 0), map);
+    State topMiddle = new State(new Coord(0, 1), map);
+    State topRight = new State(new Coord(0, 2), map);
+
+    State middleLeft = new State(new Coord(1, 0), map);
+    State middleMiddle = new State(new Coord(1, 1), map);
+    State middleRight = new State(new Coord(1, 2), map);
+
+    State bottomLeft = new State(new Coord(2, 0), map);
+    State bottomMiddle = new State(new Coord(2, 1), map);
+    State bottomRight = new State(new Coord(2, 2), map);
+
     @Test
     public void testGetSuccessorStates() {
-        Map map = Conf.valueOf("JCONF01").getMap();
-        State topLeft = new State(new Coord(0, 0), map);
-        State topMiddle = new State(new Coord(0, 1), map);
-        State topRight = new State(new Coord(0, 2), map);
-
-        State middleLeft = new State(new Coord(1, 0), map);
-        State middleMiddle = new State(new Coord(1, 1), map);
-        State middleRight = new State(new Coord(1, 2), map);
-
-        State bottomLeft = new State(new Coord(2, 0), map);
-        State bottomMiddle = new State(new Coord(2, 1), map);
-        State bottomRight = new State(new Coord(2, 2), map);
 
         State[] topLeftSuccessors = topLeft.getSuccessorStates();
         assertEquals(2, topLeftSuccessors.length);
